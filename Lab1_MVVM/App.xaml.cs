7c7c7c11bf16b1e48274e6cd90a7b1e659969402
mlakusss <mironov.aptem@yandex.ru>
2026-04-17 21:33:21 +0300

    Инициализация, пустой WPF проект

---
+++ Lab1_MVVM/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab1_MVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
